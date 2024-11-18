894c5a1d297abde72e65a0fdf083a9af5cc06ab3
eyzs22 <120159134+eyzs22@users.noreply.github.com>
2024-11-19 05:37:20 +0700

    Create getAllProductCategories.jsx

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,16 @@
+const getAllProductCategories = () => {
+  return [
+    {
+      id: 'peralatan-memasak',
+      slug: 'peralatan-memasak',
+      name: `Peralatan Memasak`,
+    },
+    {
+      id: 'peralatan-penyajian',
+      slug: 'peralatan-penyajian',
+      name: `Peralatan Penyajian`,
+    },
+  ];
+};
+
+export default getAllProductCategories;
